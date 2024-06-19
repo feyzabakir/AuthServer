37c3c05483942f96df7216fd48ee3ead7a318371
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-06-19 22:43:24 +0300

    Generic Repository implemented

--- AuthServer.Core/Repositories/IGenericRepository.cs
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -4,13 +4,13 @@ namespace AuthServer.Core.Repositories;
 
 public interface IGenericRepository<TEntity> where TEntity : class
 {
-    Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<TEntity> GetByIdAsync(int id);
 
-    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> GetAllAsync();
 
     IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
 
-    Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task AddAsync(TEntity entity);
 
     void Remove(TEntity entity);
 
